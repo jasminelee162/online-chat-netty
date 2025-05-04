4b328b7e0be770b8ca86320048d41548b92af20c
wendyjuzi <3278281361@qq.com>
2025-05-04 18:30:50 +0800

    视频通话

---
+++ src/main/java/com/cong/wego/controller/VideoCallController.java
@@ -0,0 +1,29 @@
+package com.cong.wego.controller;
+
+import com.cong.wego.model.enums.ws.WSReqTypeEnum;
+import com.cong.wego.websocket.client.VideoCallWebSocketClient;
+import com.cong.wego.websocket.handler.SignalingSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/chat")
+public class VideoCallController {
+
+    private final SignalingSocketHandler signalingSocketHandler;
+
+    @Autowired
+    public VideoCallController(SignalingSocketHandler signalingSocketHandler) {
+        this.signalingSocketHandler = signalingSocketHandler;
+    }
+
+    @PostMapping("/video/call")
+    public ResponseEntity<String> initiateVideoCall(@RequestParam String fromUserId, @RequestParam String toUserId) {
+        // 构造信令消息
+        SignalingSocketHandler.VideoSignalMessage message = new SignalingSocketHandler.VideoSignalMessage(fromUserId, toUserId, WSReqTypeEnum.VIDEO_CALL.name());
+        // 使用 SignalingSocketHandler 发送信令
+        signalingSocketHandler.sendSignalMessage(message);
+        return ResponseEntity.ok("视频通话请求已发送");
+    }
+}
